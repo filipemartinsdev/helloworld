68499588ccd1fbd0535534064b36ad4856699789
filipemartinsdev <dev.filipemartins@gmail.com>
2026-02-04 13:17:54 -0300

    first commit

---
+++ src/main/java/io/github/filipemartinsdev/helloworld/Hello.java
@@ -0,0 +1,10 @@
+package io.github.filipemartinsdev.helloworld;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class Hello {
+    public void print(){
+        System.out.println("Hello world");
+    };
+}
